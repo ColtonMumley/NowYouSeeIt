cd14f3c6ea5dc3088b9c14ff8900da2980ee7078
ColtonMumley <colton12@vt.edu>
2019-07-27 19:31:24 -0400

    added finish orb

---
+++ Assets/Scripts/ZoomOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour
+{
+
+    public Camera zoomCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1)) {
+            SwitchCamera();
+        }
+    }
+
+    void SwitchCamera() {
+        transform.position = zoomCam.transform.position;
+        GetComponent<Camera>().orthographicSize = zoomCam.orthographicSize;
+    }
+
+}
