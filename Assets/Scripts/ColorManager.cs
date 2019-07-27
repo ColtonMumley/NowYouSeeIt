8f26b806a5f5c37e39a9c9398f3d5e2774028bff
ColtonMumley <colton12@vt.edu>
2019-07-27 16:17:23 -0400

    first commit

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        disablePlatforms(Color.black, Color.white);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            SwitchColors();
+        }
+            
+    }
+
+    // Changes Player and Background colors
+    void SwitchColors() {
+        if (player.GetComponent<SpriteRenderer>().color == Color.white) {
+            player.GetComponent<SpriteRenderer>().color = Color.black;
+            disablePlatforms(Color.white, Color.black);
+        } else {
+            player.GetComponent<SpriteRenderer>().color = Color.white;
+            disablePlatforms(Color.black, Color.white);
+        }
+        if (mainCam.backgroundColor == Color.white) {
+            mainCam.backgroundColor = Color.black;
+        } else {
+            mainCam.backgroundColor = Color.white;
+        }
+    }
+
+    // Disables colliders on any platform with color of disableColor,
+    // and enables colliders on any platform with color of enableColor
+    void disablePlatforms(Color disableColor, Color enableColor) {
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Ground");
+        foreach(GameObject platform in platforms) {
+            if (platform.GetComponent<SpriteRenderer>().color == disableColor) {
+                platform.GetComponent<EdgeCollider2D>().enabled = false;
+            } else {
+                platform.GetComponent<EdgeCollider2D>().enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 50f;
+    public float jumpForce = 10f;
+    public float fallMultiplier = 10f;
+
+    private Rigidbody2D rb;
+    private float hInput;
+    private bool jumpInput = false;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        hInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && rb.velocity.y < 0.001)
+        {
+            jumpInput = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(hInput * speed * Time.deltaTime, rb.velocity.y);
+        if (jumpInput && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpInput = false;
+            isGrounded = false;
+        }
+        if (rb.velocity.y < 0)
+        {
+            float newVel = rb.velocity.y + (-9.81f * fallMultiplier * Time.fixedDeltaTime);
+            rb.velocity = new Vector2(rb.velocity.x, newVel);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground" && isGrounded == false) {
+            isGrounded = true;
+        }
+    }
+}
