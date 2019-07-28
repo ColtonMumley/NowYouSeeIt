96fb71600879459fead456cea9659eb74792e23b
ColtonMumley <colton12@vt.edu>
2019-07-27 21:49:10 -0400

    Implemented respawn

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public float threshold = -50f;
+    public Vector3 spawnPoint;
+    public ColorManager colorManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold) {
+            colorManager.Reset();
+            transform.position = spawnPoint;
+        }
+    }
+}
